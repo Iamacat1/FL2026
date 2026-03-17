fix: computation cost

A_Client.py
@@ -22,6 +22,7 @@ from A_utils import *
 class Client:
     def __init__(self, index, config, private_key, public_key_list):
         self.sign_time = 0
+        self.r3_time = 0
         self.verify_time = 0
         self.verify_correct_time = 0
         self.index = index
@@ -42,6 +43,16 @@ class Client:
         signature = self.private_key.sign(self.grad_iter.tobytes())
         self.sign_time = time.time() - start
         return self.grad_iter, signature
+
+    def verify_as(self, vs_agg_sig):
+        start = time.time()
+        agg_sig, vs_sig = vs_agg_sig[0], vs_agg_sig[1]
+        # 验证VS给聚合结果的签名
+        verify(self.public_key_list[self.client_num], vs_sig, agg_sig[0].tobytes() + agg_sig[1])
+        # 验证AS给聚合结果的签名
+        verify(self.public_key_list[self.client_num+1], agg_sig[1], agg_sig[0].tobytes())
+        self.r3_time = time.time() - start
+
 
     def verify(self, p_sig, vs_grad, signature, agg_sig):
         start = time.time()
@@ -95,21 +106,33 @@ class Client:
     def verify_correct_agg(self, agg_sig, flag):
         start = time.time()
         agg_grad, agg_signature  = agg_sig[0], agg_sig[1]
-        if flag == "case2_1_1":
+        if flag == "case2_1_1" or flag == "case2-2-1":
             verify(self.public_key_list[self.client_num+1], agg_signature, agg_grad.tobytes())
         else:
             verify(self.public_key_list[self.client_num], agg_signature, agg_grad.tobytes())
         self.verify_correct_time = time.time() - start
 
     def get_time(self):
-        self.time = [self.sign_time, self.verify_time + self.verify_correct_time]
+        self.time = [self.sign_time, self.r3_time, self.verify_time + self.verify_correct_time]
         print(f"客户端{self.index}各阶段时间：{self.time}")
-        print(f"客户端{self.index}总时间：{sum(self.time)}")
-        return self.time
+        self.total_time = sum(self.time)+(self.client_num*2+5)*0.001
+        print(f"客户端{self.index}总时间：{self.total_time}")
+        return self.total_time
 
 if __name__ == '__main__':
+    # start = time.time()
+    # for i in range(50):
+    #     prg(i, 50000)    
+    # sign_time = time.time() - start
+    # print(f"生成随机数时间：{sign_time}")
+    grad_list = []
+    for i in range(100):
+        grad = np.random.randint(1, MAX_INPUT, 100000)
+        grad_list.append(grad)
     start = time.time()
-    for i in range(50):
-        prg(i, 50000)    
-    sign_time = time.time() - start
-    print(f"生成随机数时间：{sign_time}")
+    for i in range(100):
+        if i == 0:
+            agg_grad = grad_list[i]
+        else:
+            agg_grad = agg_grad + grad_list[i]
+    print(f"求和时间：{time.time() - start}")